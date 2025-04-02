916a693b495d2e4c0e2f290b14d14a6273c9c623
Amindu Abeydeera <aminduab7@gmail.com>
2025-04-02 14:48:36 -0400

    inital

--- js/script.js
+++ js/script.js
@@ -1,231 +1,107 @@
-let allData = [];
-let xVar = 'temperature', yVar = 'precipitation', sizeVar = 'windSpeed', targetMonth = 1, targetDay=1, stateVar='GU';
-let xScale, yScale, sizeScale;
-
-const margin = { top: 80, right: 60, bottom: 60, left: 100 };
-const width = 800 - margin.left - margin.right;
-const height = 600 - margin.top - margin.bottom;
-
-const options = ['temperature', 'windDirection', 'precipitation', 'windSpeed'];
-
-const t = 1000; // Transition duration
-
-// Create SVG
-const svg = d3.select('#vis')
-    .append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
-    .append('g')
-    .attr('transform', `translate(${margin.left},${margin.top})`);
-
-function init() {
-    d3.csv('data/weather.csv', function(d) {
-        return {
-            date: +d.date,
-            year: +(d.date.substring(0, 4)),
-            month: +(d.date.substring(4, 6)),
-            day: +(d.date.substring(6)),
-            temperature: +d.TAVG,
-            windDirection: +d.WDF5,
-            windSpeed: +d.AWND,
-            precipitation: +d.PRCP,
-            station: d.station,
-            state: d.state
-        };
-    })
-    .then(data => {
-        allData = data;
-        console.log(allData);
-        setupSelector();
-        updateAxes();
-        updateVis();
-    })
-    .catch(error => console.error('Error loading data:', error));
-}
-
-window.addEventListener('load', init);
-
-function setupSelector() {
-    var slider = d3
-        .sliderHorizontal()
-        .min(d3.min(allData.map(d => d.month))) // setup the range
-        .max(d3.max(allData.map(d => d.month))+1) // setup the range
-        .step(1) // 1 day step
-        .width(width)
-        .displayValue(false)
-        .on('onchange', (val) => {
-            console.log(val);
-            targetMonth = val; // Update the date
-            updateVis(); // Refresh the chart
-        });
-    
-    d3.select('#slider')
-        .append('svg')
-        .attr('width', width)
-        .attr('height', 100)
-        .append('g')
-        .attr('transform', 'translate(30,30)')
-        .call(slider);
-
-    var daySlider = d3
-    .sliderHorizontal()
-    .min(d3.min(allData.map(d => d.day))) // setup the range
-    .max(d3.max(allData.map(d => d.day))) // setup the range
-    .step(1) // 1 day step
-    .width(width)
-    .displayValue(false)
-    .on('onchange', (val) => {
-        console.log(val);
-        targetDay = val; // Update the date
-        updateVis(); // Refresh the chart
+/***** WHITE HAT VISUALIZATION: Scatter Plot *****/
+d3.csv("data/census_tracts.csv").then(function(data) {
+    data.forEach(function(d) {
+      d.median_income = +d.median_income;
+      d.median_home_value = +d.median_home_value;
     });
-    
-    d3.select('#daySlider')
-        .append('svg')
-        .attr('width', width)
-        .attr('height', 100)
-        .append('g')
-        .attr('transform', 'translate(30,30)')
-        .call(daySlider);
-    
-    d3.selectAll('.variable')
-        .each(function() {
-            d3.select(this).selectAll('myOptions')
-                .data(options)
-                .enter()
-                .append('option')
-                .text(d => d)
-                .attr("value", d => d);
-        })
-        .on("change", function(event) {
-            const selectedId = d3.select(this).property("id");
-            const selectedValue = d3.select(this).property("value");
-
-            if (selectedId === "xVariable") {
-                xVar = selectedValue;
-            } else if (selectedId === "yVariable") {
-                yVar = selectedValue;
-            } else if (selectedId === "sizeVariable") {
-                sizeVar = selectedValue;
-            }
-
-            updateAxes();
-            updateVis();
-        });
-        d3.select('#xVariable').property('value', xVar)
-        d3.select('#yVariable').property('value', yVar)
-        d3.select('#sizeVariable').property('value',sizeVar)
-
-        const stateSet = new Set(allData.map(d => d.state));
-        d3.select('#stateVariable')
-            .selectAll('myOptions')
-            .data(stateSet)
-            .enter()
-            .append('option')
-            .text(d => d)
-            .attr("value", d => d);
-        d3.select('#stateVariable').on("change", function() {
-            const selectedState = d3.select(this).property("value");
-            stateVar=selectedState
-            updateAxes();
-            updateVis();
-        })
-        d3.select('#stateVariable').property('value', stateVar);
-    }
-
-function updateAxes() {
-    svg.selectAll('.axis').remove();
-    svg.selectAll('.labels').remove();
-
-    xScale = d3.scaleLinear()
-        .domain([d3.min(allData, d => d[xVar]), d3.max(allData, d => d[xVar])])
-        .range([0, width]);
-    const xAxis = d3.axisBottom(xScale);
-
-    svg.append("g")
-        .attr("class", "axis")
-        .attr("transform", `translate(0,${height})`)
-        .call(xAxis);
-
-    yScale = d3.scaleLinear()
-        .domain([d3.min(allData, d => d[yVar]), d3.max(allData, d => d[yVar])])
-        .range([height, 0]);
-
-    const yAxis = d3.axisLeft(yScale);
-    
-    svg.append('g')
-        .attr('class', 'axis')
-        .attr('transform', `translate(0, 0)`)
-        .call(yAxis);
-
-    sizeScale = d3.scaleSqrt()
-        .domain([0, d3.max(allData, d => d[sizeVar])])
-        .range([5, 20]);
-
+  
+    var svg = d3.select("#white-hat svg"),
+        margin = {top: 20, right: 30, bottom: 50, left: 60},
+        width = +svg.attr("width") - margin.left - margin.right,
+        height = +svg.attr("height") - margin.top - margin.bottom;
+  
+    var g = svg.append("g")
+               .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  
+    var x = d3.scaleLinear()
+              .domain([0, d3.max(data, d => d.median_income)]).nice()
+              .range([0, width]);
+    var y = d3.scaleLinear()
+              .domain([0, d3.max(data, d => d.median_home_value)]).nice()
+              .range([height, 0]);
+  
+    g.append("g")
+     .attr("transform", "translate(0," + height + ")")
+     .call(d3.axisBottom(x));
+    g.append("g")
+     .call(d3.axisLeft(y));
+  
     svg.append("text")
-        .attr("x", width / 2)
-        .attr("y", height + margin.bottom - 20)
-        .attr("text-anchor", "middle")
-        .text(xVar)
-        .attr('class', 'labels');
-
+       .attr("transform", "translate(" + (margin.left + width/2) + " ," + (height + margin.top + 40) + ")")
+       .style("text-anchor", "middle")
+       .text("Median Income (dollars)");
     svg.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("x", -height / 2)
-        .attr("y", -margin.left + 40)
-        .attr("text-anchor", "middle")
-        .text(yVar)
-        .attr('class', 'labels');
-}
-
-function updateVis() {
-    let currentData = allData.filter(d => d.month === targetMonth && d.state === stateVar);
-
-    svg.selectAll('.points')
-        .data(currentData, d => d.date)
-        .join(
-            function(enter) {
-                return enter
-                    .append('circle')
-                    .attr('class', 'points')
-                    .attr('cx', d => xScale(d[xVar]))
-                    .attr('cy', d => yScale(d[yVar]))
-                    .attr('r', d => sizeScale(d[sizeVar]))
-                    .style('opacity', .5)
-                    .on('mouseover', function(event, d) {
-                        d3.select(this)
-                            .style("fill", "orange")
-                            .style("opacity", 1);
-
-                        d3.select('#tooltip')
-                            .style("display", 'block')
-                            .html(`<strong>Date: ${d.month}-${d.day}-${d.year}</strong><br/>${xVar}: ${d[xVar]}<br/>${yVar}: ${d[yVar]}`)
-                            .style("left", (event.pageX + 20) + "px")
-                            .style("top", (event.pageY - 28) + "px");
-                    })
-                    .on("mouseout", function() {
-                        d3.select(this)
-                            .style("opacity", 0.5);
-
-                        d3.select('#tooltip')
-                            .style('display', 'none');
-                    })
-                    .attr('r', 0)
-                    .transition(t)
-                    .attr('r', d => sizeScale(d[sizeVar]));
-            },
-            function(update) {
-                return update
-                    .transition(t)
-                    .attr('cx', d => xScale(d[xVar]))
-                    .attr('cy', d => yScale(d[yVar]))
-                    .attr('r', d => sizeScale(d[sizeVar]));
-            },
-            function(exit) {
-                exit
-                    .transition(t)
-                    .attr('r', 0)
-                    .remove();
-            }
-        );
-}
+       .attr("transform", "rotate(-90)")
+       .attr("y", margin.left - 50)
+       .attr("x", 0 - (height / 2) - margin.top)
+       .attr("dy", "1em")
+       .style("text-anchor", "middle")
+       .text("Median Home Value (dollars)");
+  
+    var tooltip = d3.select("body").append("div")
+                    .attr("class", "tooltip");
+  
+    g.selectAll("circle")
+     .data(data)
+     .enter().append("circle")
+       .attr("cx", d => x(d.median_income))
+       .attr("cy", d => y(d.median_home_value))
+       .attr("r", 4)
+       .attr("fill", "steelblue")
+       .on("mouseover", function(event, d) {
+          tooltip.transition().duration(200).style("opacity", 0.9);
+          tooltip.html("Census Tract: " + d.geoid + "<br/>" +
+                       "Income: " + d.median_income + "<br/>" +
+                       "Home Value: " + d.median_home_value)
+                 .style("left", (event.pageX + 5) + "px")
+                 .style("top", (event.pageY - 28) + "px");
+       })
+       .on("mouseout", function() {
+          tooltip.transition().duration(500).style("opacity", 0);
+       });
+  });
+  
+  /***** BLACK HAT VISUALIZATION: Misleading Bar Chart *****/
+  d3.csv("data/census_tracts.csv").then(function(data) {
+    data.forEach(function(d) {
+      d.total_population = +d.total_population;
+    });
+  
+    data.sort((a, b) => b.total_population - a.total_population);
+    var filteredData = data.slice(0, 10);
+  
+    var svg = d3.select("#black-hat svg"),
+        margin = {top: 20, right: 30, bottom: 50, left: 60},
+        width = +svg.attr("width") - margin.left - margin.right,
+        height = +svg.attr("height") - margin.top - margin.bottom;
+  
+    var g = svg.append("g")
+               .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  
+    var y = d3.scaleLinear()
+              .domain([d3.min(filteredData, d => d.total_population), d3.max(filteredData, d => d.total_population)])
+              .range([height, 0]);
+    var x = d3.scaleBand()
+              .domain(filteredData.map(d => d.geoid))
+              .range([0, width])
+              .padding(0.1);
+  
+    g.append("g")
+     .attr("transform", "translate(0," + height + ")")
+     .call(d3.axisBottom(x));
+    g.append("g")
+     .call(d3.axisLeft(y));
+  
+    g.selectAll(".bar")
+     .data(filteredData)
+     .enter().append("rect")
+       .attr("class", "bar")
+       .attr("x", d => x(d.geoid))
+       .attr("y", d => y(d.total_population))
+       .attr("width", x.bandwidth())
+       .attr("height", d => height - y(d.total_population))
+       .attr("fill", "tomato")
+       .append("title")  
+       .text(d => "Population: " + d.total_population);
+  });
+  
